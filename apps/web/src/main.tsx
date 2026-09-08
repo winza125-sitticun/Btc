8f074840297e2a5ce6b961feb14563c798db2246
winza125-sitticun <winza125@gmail.com>
2026-09-08 10:04:48 +0700

    feat: add React entrypoint

---
+++ apps/web/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles.css'
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js'))
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
